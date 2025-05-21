2f99a2ba223406cb9a3a419b64de27049ce48cf8
LuisTangarife <aux.reg.academicoii@autonoma.edu.co>
2025-05-21 13:42:00 -0500

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -34,7 +34,11 @@ function appendMessage(text, sender) {
     hablar(text);
   }
 }
-  
+  function limpiarHistorial() {
+  const chatBox = document.getElementById('chat-box');
+  chatBox.innerHTML = '<div class="bot-message">🧠 Historial borrado. ¿En qué más puedo ayudarte?</div>';
+  document.getElementById('botones-dinamicos').innerHTML = '';
+}
 // Simula una respuesta y muestra botones si hay coincidencia en el JSON
 function sendMessage() {
   const input = document.getElementById("user-input");
